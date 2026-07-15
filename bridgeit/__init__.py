43d19235b7ec9aaf4e1d2be360a38cf66b097ad1
Martina Fava <martina.fava3@studio.unibo.it>
2026-07-15 19:08:55 +0200

    Remove unused template placeholder and update project metadata

--- bridgeit/__init__.py
+++ bridgeit/__init__.py
@@ -1,25 +1,6 @@
 import logging
 
-
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("bridgeit")
 
-
-# this is the initial module of your app
-# this is executed whenever some client-code is calling `import bridgeit` or `from bridgeit import ...`
-# put your main classes here, eg:
-class MyClass:
-    def my_method(self):
-        return "Hello World"
-
-
-def main():
-    # this is the main module of your app
-    # it is only required if your project must be runnable
-    # this is the script to be executed whenever some users writes `python -m bridgeit` on the command line, eg.
-    x = MyClass().my_method()
-    print(x)
-
-
-# let this be the last line of this file
 logger.info("bridgeit loaded")
